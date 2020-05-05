e1c465d91c94f96d9313c6c0fb5fd6a22823e710
Jai Castle <jaicastle0@gmail.com>
2020-05-05 18:26:39 +0800

    Organised air-brakes.c and run-simulations.sh

--- air-brakes.c
+++ air-brakes.c
@@ -2,35 +2,35 @@
 
 #define OPTLIST "s"
 
+void runSimulations()
+{
+	int maxTime = 100;
+	int maxHeight = 10000;
+	for (size_t i = 0; i <= maxTime / 2; i++)
+	{
+		int currentHeight = getFakeCurrentHeightQuadratic(i, maxTime, maxHeight);
+		int PIDResult = PIDController(currentHeight);
+		printf("%i,%i\n", currentHeight, PIDResult);
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	printf("--- UWA Aerospace: Air Brakes ---\n");
 	int option = -1;
-	bool simulate = false;
 	while ((option = getopt(argc, argv, OPTLIST)) != -1)
 	{
 		switch (option)
 		{
 		case 's':
 		{
-			simulate = true;
+			runSimulations();
 			break;
 		}
 		default:
 		{
 			break;
 		}
-		}
-	}
-	if (simulate)
-	{
-		int maxTime = 100;
-		int maxHeight = 10000;
-		for (size_t i = 0; i <= maxTime / 2; i++)
-		{
-			int currentHeight = getFakeCurrentHeightQuadratic(i, maxTime, maxHeight);
-			int PIDResult = PIDController(currentHeight);
-			printf("%i,%i\n", currentHeight, PIDResult);
 		}
 	}
 
--- simulations/run-simulations.sh
+++ simulations/run-simulations.sh
@@ -7,7 +7,7 @@ echo
 
 # Run the simulations
 echo "height,brake-angle" > temp_output.csv
-../air-brakes -s | grep -vi "Aerospace" >> temp_output.csv
+../air-brakes -s | grep "[0-9]*\,[0-9]*" >> temp_output.csv
 python3 graph-results.py temp_output.csv
 rm temp_output.csv 
 echo "Simulations complete."
