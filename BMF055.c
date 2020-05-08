f1863f7ced92d0fd1f66694f47a65c2ef26ad56d
Jai Castle <jaicastle0@gmail.com>
2020-05-08 22:51:20 +0800

    Uploaded simulation data, file input preparation

--- BMF055.c
+++ BMF055.c
@@ -11,3 +11,33 @@ int getFakeCurrentHeightQuadratic(int time, int maxTime, int maxHeight)
     // Quadratic: roots of 0 and maxTime, height of h
     return -(4 * time * (time - maxTime) * maxHeight) / (pow(maxTime, 2.0));
 }
+
+int[] initialiseFile(char *fileName)
+{
+    FILE *file;
+    file = fopen(fileName, "r");
+
+    // Modify file
+    if (!file)
+    {
+        perror("Could not open file.");
+    }
+
+    //Get each line of the file
+    char line[1000];
+    while (fgets(line, sizeof line, file) != NULL)
+    {
+        char *ch = line;
+        // TODO: read in the simulation data to use in place of a quadratic
+    }
+}
+
+int getFakeCurrentHeightFromFile(int time, int)
+{
+    static bool initialised = false;
+    static int[] heights;
+
+    // TODO: initialiseFile() if required, read the appropriate data
+
+    initialised = true;
+}
